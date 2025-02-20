f53d45f81318327660a99ced205a21f735117600
Dinh Kim Thach <dinhkimthach@gmail.com>
2025-02-20 11:04:06 +0700

    first commit

---
+++ Img2Word/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Img2Word
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lvImages = new System.Windows.Forms.ListView();
+            this.nudQuality = new System.Windows.Forms.NumericUpDown();
+            this.btnExportWord = new System.Windows.Forms.Button();
+            this.btnExportPDF = new System.Windows.Forms.Button();
+            this.btnExportAsImg = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnOpenFoler = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbOutputSize = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuality)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lvImages
+            // 
+            this.lvImages.AllowDrop = true;
+            this.lvImages.Location = new System.Drawing.Point(12, 12);
+            this.lvImages.Name = "lvImages";
+            this.lvImages.Size = new System.Drawing.Size(323, 356);
+            this.lvImages.TabIndex = 0;
+            this.lvImages.UseCompatibleStateImageBehavior = false;
+            this.lvImages.DragDrop += new System.Windows.Forms.DragEventHandler(this.LvImages_DragDrop);
+            this.lvImages.DragEnter += new System.Windows.Forms.DragEventHandler(this.LvImages_DragEnter);
+            // 
+            // nudQuality
+            // 
+            this.nudQuality.Location = new System.Drawing.Point(340, 12);
+            this.nudQuality.Name = "nudQuality";
+            this.nudQuality.Size = new System.Drawing.Size(96, 20);
+            this.nudQuality.TabIndex = 1;
+            this.nudQuality.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // btnExportWord
+            // 
+            this.btnExportWord.Location = new System.Drawing.Point(340, 39);
+            this.btnExportWord.Name = "btnExportWord";
+            this.btnExportWord.Size = new System.Drawing.Size(96, 23);
+            this.btnExportWord.TabIndex = 2;
+            this.btnExportWord.Text = "Export as Word";
+            this.btnExportWord.UseVisualStyleBackColor = true;
+            this.btnExportWord.Click += new System.EventHandler(this.btnExportWord_Click);
+            // 
+            // btnExportPDF
+            // 
+            this.btnExportPDF.Location = new System.Drawing.Point(340, 68);
+            this.btnExportPDF.Name = "btnExportPDF";
+            this.btnExportPDF.Size = new System.Drawing.Size(96, 23);
+            this.btnExportPDF.TabIndex = 3;
+            this.btnExportPDF.Text = "Export as PDF";
+            this.btnExportPDF.UseVisualStyleBackColor = true;
+            this.btnExportPDF.Click += new System.EventHandler(this.btnExportPDF_Click);
+            // 
+            // btnExportAsImg
+            // 
+            this.btnExportAsImg.Location = new System.Drawing.Point(340, 97);
+            this.btnExportAsImg.Name = "btnExportAsImg";
+            this.btnExportAsImg.Size = new System.Drawing.Size(96, 23);
+            this.btnExportAsImg.TabIndex = 4;
+            this.btnExportAsImg.Text = "Export as image";
+            this.btnExportAsImg.UseVisualStyleBackColor = true;
+            this.btnExportAsImg.Click += new System.EventHandler(this.btnExportAsImg_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Enabled = false;
+            this.txtPath.Location = new System.Drawing.Point(13, 378);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(322, 20);
+            this.txtPath.TabIndex = 5;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(341, 378);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(93, 23);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnOpenFoler
+            // 
+            this.btnOpenFoler.Location = new System.Drawing.Point(341, 345);
+            this.btnOpenFoler.Name = "btnOpenFoler";
+            this.btnOpenFoler.Size = new System.Drawing.Size(93, 23);
+            this.btnOpenFoler.TabIndex = 7;
+            this.btnOpenFoler.Text = "Open folder";
+            this.btnOpenFoler.UseVisualStyleBackColor = true;
+            this.btnOpenFoler.Click += new System.EventHandler(this.btnOpenFoler_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 402);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Output size (approximate):";
+            // 
+            // lbOutputSize
+            // 
+            this.lbOutputSize.Location = new System.Drawing.Point(147, 402);
+            this.lbOutputSize.Name = "lbOutputSize";
+            this.lbOutputSize.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lbOutputSize.Size = new System.Drawing.Size(58, 23);
+            this.lbOutputSize.TabIndex = 9;
+            this.lbOutputSize.Text = "0 MB";
+            this.lbOutputSize.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 424);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbOutputSize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpenFoler);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnExportAsImg);
+            this.Controls.Add(this.btnExportPDF);
+            this.Controls.Add(this.btnExportWord);
+            this.Controls.Add(this.nudQuality);
+            this.Controls.Add(this.lvImages);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Img2Doc 1.0 (by dkt999)";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuality)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvImages;
+        private System.Windows.Forms.NumericUpDown nudQuality;
+        private System.Windows.Forms.Button btnExportWord;
+        private System.Windows.Forms.Button btnExportPDF;
+        private System.Windows.Forms.Button btnExportAsImg;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnOpenFoler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbOutputSize;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Img2Word/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using Xceed.Words.NET;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+namespace Img2Word
+{
+    public partial class Form1 : Form
+    {
+        private List<string> imagePaths = new List<string>();
+        private ImageList imageList = new ImageList();
+        string defaultPath = Application.StartupPath + @"\Output";
+        public Form1()
+        {
+            InitializeComponent();
+            lvImages.View = View.LargeIcon;
+            lvImages.LargeImageList = imageList;
+            imageList.ColorDepth = ColorDepth.Depth32Bit;
+            imageList.ImageSize = new Size(100, 100); // Kích thước ảnh nhỏ
+            lvImages.AllowDrop = true;
+            txtPath.Text = defaultPath;
+        }
+        private void LvImages_DragEnter(object sender, DragEventArgs e)
+        {
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+        private void LvImages_DragDrop(object sender, DragEventArgs e)
+        {
+            
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+               
+                if (Path.GetExtension(file).ToLower() == ".jpg" || Path.GetExtension(file).ToLower() == ".png")
+                {
+                    imagePaths.Add(file);
+
+                    // Load ảnh vào ImageList
+                    Image img = ResizeImageKeepAspectRatio(Image.FromFile(file), 100, 100);
+                    imageList.Images.Add(file, img);
+
+                    // Thêm ảnh vào ListView
+                    ListViewItem item = new ListViewItem(Path.GetFileName(file))
+                    {
+                        ImageKey = file // Gán ảnh từ ImageList
+                    };
+                    lvImages.Items.Add(item);
+                }
+            }
+            if (imagePaths.Count > 0)
+            {
+                string tempFolder = Path.Combine(Path.GetTempPath(), "CompressedImages");
+                try
+                {
+                    if (Directory.Exists(tempFolder))
+                    {
+                        Directory.Delete(tempFolder, true); // Xóa thư mục và tất cả file bên trong
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi xóa thư mục tạm: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                int quality = (int)nudQuality.Value;
+                Directory.CreateDirectory(tempFolder);
+
+                List<string> compressedImages = new List<string>();
+
+                foreach (string imgPath in imagePaths)
+                {
+                    try
+                    {
+                        string outputFile = Path.Combine(tempFolder, Path.GetFileName(imgPath));
+                        CompressImage(imgPath, outputFile, quality);
+                        compressedImages.Add(outputFile);
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show("Lỗi khi xóa thư mục tạm: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                long totalSize = GetDirectorySize(tempFolder);
+                double sizeInMB = Math.Round(totalSize / (1024.0 * 1024.0), 1);
+
+                lbOutputSize.Text = sizeInMB.ToString() + " MB";
+            }
+        }
+        private void CompressImage(string inputPath, string outputPath, int quality)
+        {
+            using (Bitmap bmp = new Bitmap(inputPath))
+            {
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+                EncoderParameters encoderParams = new EncoderParameters(1);
+                encoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+                try
+                {
+                    bmp.Save(outputPath, jpgEncoder, encoderParams);
+                }
+                catch(Exception ex)
+                { }
+            }
+        }
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            foreach (var codec in ImageCodecInfo.GetImageEncoders())
+            {
+                if (codec.FormatID == format.Guid)
+                    return codec;
+            }
+            return null;
+        }
+        private void ExportToWord(List<string> images)
+        {
+            string Filename = "Img2Word_" + DateTime.Now.ToFileTime();
+            using (var doc = DocX.Create(txtPath.Text.ToString() + @"\" + Filename + ".docx"))
+            {
+                // Kích thước trang A4: 595 x 842 px (8.27 x 11.69 inch, 72 dpi)
+                float pageWidth = 595f;
+                float pageHeight = 842f;
+
+                // Lề an toàn khi in (~72px mỗi bên)
+                float margin = 72f;
+                float safeWidth = pageWidth - (2 * margin);
+                float safeHeight = pageHeight - (2 * margin);
+
+                foreach (string imagePath in images)
+                {
+                    using (Image img = Image.FromFile(imagePath))
+                    {
+                        float imgWidth = img.Width;
+                        float imgHeight = img.Height;
+
+                        // Tính tỷ lệ để ảnh vừa trong vùng in
+                        float scale = Math.Min(safeWidth / imgWidth, safeHeight / imgHeight);
+                        float newWidth = imgWidth * scale;
+                        float newHeight = imgHeight * scale;
+
+                        // Thêm ảnh vào Word
+                        var image = doc.AddImage(imagePath);
+                        var picture = image.CreatePicture((int)newHeight, (int)newWidth);
+
+                        var p = doc.InsertParagraph();
+                        p.AppendPicture(picture);
+                        p.Append("\n"); // Xuống dòng cho ảnh sau
+
+                        // Căn giữa ảnh trong trang
+                        p.Alignment = Xceed.Document.NET.Alignment.center;
+                    }
+                }
+                doc.Save();
+            }
+            MessageBox.Show("Đã xong chúc vui!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void btnExportWord_Click(object sender, EventArgs e)
+        {
+            if (imagePaths.Count == 0)
+            {
+                MessageBox.Show("Vui lòng thêm ảnh vào danh sách.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int quality = (int)nudQuality.Value;
+            string tempFolder = Path.Combine(Path.GetTempPath(), "CompressedImages");
+            Directory.CreateDirectory(tempFolder);
+
+            List<string> compressedImages = new List<string>();
+
+            foreach (string imgPath in imagePaths)
+            {
+                string outputFile = Path.Combine(tempFolder, Path.GetFileName(imgPath));
+                CompressImage(imgPath, outputFile, quality);
+                compressedImages.Add(outputFile);
+            }
+
+            ExportToWord(compressedImages);
+        }
+        private Image ResizeImageKeepAspectRatio(Image img, int maxWidth, int maxHeight)
+        {
+            // Tính toán tỉ lệ mới
+            float ratioX = (float)maxWidth / img.Width;
+            float ratioY = (float)maxHeight / img.Height;
+            float ratio = Math.Min(ratioX, ratioY); // Giữ tỉ lệ
+
+            int newWidth = (int)(img.Width * ratio);
+            int newHeight = (int)(img.Height * ratio);
+
+            // Tạo nền trong suốt
+            Bitmap bmp = new Bitmap(maxWidth, maxHeight);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.Transparent); // Nền trong suốt
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                // Tính vị trí căn giữa
+                int x = (maxWidth - newWidth) / 2;
+                int y = (maxHeight - newHeight) / 2;
+                g.DrawImage(img, x, y, newWidth, newHeight);
+            }
+            return bmp;
+        }
+        public void ExportImagesToPdf(string outputFile, List<string> imagePaths)
+        {
+            PdfDocument document = new PdfDocument();
+
+            // Kích thước trang A4 (595 x 842 px tại 72 dpi)
+            double pageWidth = 595;
+            double pageHeight = 842;
+
+            // Lề an toàn khi in (~72px mỗi bên)
+            double margin = 72;
+            double safeWidth = pageWidth - (2 * margin);
+            double safeHeight = pageHeight - (2 * margin);
+
+            foreach (string imagePath in imagePaths)
+            {
+                PdfPage page = document.AddPage();
+                XGraphics gfx = XGraphics.FromPdfPage(page);
+
+                using (Image img = Image.FromFile(imagePath))
+                {
+                    double imgWidth = img.Width;
+                    double imgHeight = img.Height;
+
+                    // Tính tỷ lệ để ảnh vừa với trang mà không bị méo
+                    double scale = Math.Min(safeWidth / imgWidth, safeHeight / imgHeight);
+                    double newWidth = imgWidth * scale;
+                    double newHeight = imgHeight * scale;
+
+                    // Căn giữa ảnh
+                    double x = (pageWidth - newWidth) / 2;
+                    double y = (pageHeight - newHeight) / 2;
+
+                    // Vẽ ảnh vào PDF
+                    XImage xImage = XImage.FromFile(imagePath);
+                    gfx.DrawImage(xImage, x, y, newWidth, newHeight);
+                }
+            }
+            document.Save(outputFile);
+            MessageBox.Show("Đã xong chúc vui!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void btnExportPDF_Click(object sender, EventArgs e)
+        {
+            if (imagePaths.Count == 0)
+            {
+                MessageBox.Show("Vui lòng thêm ảnh vào danh sách.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int quality = (int)nudQuality.Value;
+            string tempFolder = Path.Combine(Path.GetTempPath(), "CompressedImages");
+            Directory.CreateDirectory(tempFolder);
+
+            List<string> compressedImages = new List<string>();
+
+            foreach (string imgPath in imagePaths)
+            {
+                string outputFile = Path.Combine(tempFolder, Path.GetFileName(imgPath));
+                CompressImage(imgPath, outputFile, quality);
+                compressedImages.Add(outputFile);
+            }
+            string Filename = "Img2Word_" + DateTime.Now.ToFileTime();
+            ExportImagesToPdf(txtPath.Text.ToString() + @"\"+ Filename + ".pdf", compressedImages);
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+        private void btnOpenFoler_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                MessageBox.Show("Vui lòng nhập đường dẫn!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (Directory.Exists(path)) // Nếu là thư mục
+            {
+                System.Diagnostics.Process.Start("explorer.exe", path);
+            }
+            else if (File.Exists(path)) // Nếu là file, mở Explorer và chọn file đó
+            {
+                System.Diagnostics.Process.Start("explorer.exe", $"/select,\"{path}\"");
+            }
+            else
+            {
+                MessageBox.Show("Đường dẫn không hợp lệ!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnExportAsImg_Click(object sender, EventArgs e)
+        {
+            if (imagePaths.Count == 0)
+            {
+                MessageBox.Show("Vui lòng thêm ảnh vào danh sách.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int quality = (int)nudQuality.Value;
+            ///string tempFolder = Path.Combine(Path.GetTempPath(), "CompressedImages");
+            string OutputFolder = txtPath.Text.ToString() + @"\" + "Img2Word_" + DateTime.Now.ToFileTime();
+            Directory.CreateDirectory(OutputFolder);
+
+            List<string> compressedImages = new List<string>();
+
+            foreach (string imgPath in imagePaths)
+            {
+                string outputFile = Path.Combine(OutputFolder, Path.GetFileName(imgPath));
+                CompressImage(imgPath, outputFile, quality);
+                compressedImages.Add(outputFile);
+            }
+            MessageBox.Show("Đã xong chúc vui!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private long GetDirectorySize(string folderPath)
+        {
+            long totalSize = 0;
+
+            try
+            {
+                // Lấy tất cả file trong thư mục
+                string[] files = Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories);
+
+                foreach (string file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    totalSize += fileInfo.Length; // Cộng dồn kích thước file
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tính dung lượng thư mục: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return totalSize;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lvImages.Items.Clear();
+            imageList.Images.Clear();
+            imagePaths.Clear();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string tempFolder = Path.Combine(Path.GetTempPath(), "CompressedImages");
+            try
+            {
+                if (Directory.Exists(tempFolder))
+                {
+                    Directory.Delete(tempFolder, true); // Xóa thư mục và tất cả file bên trong
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi xóa thư mục tạm: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
